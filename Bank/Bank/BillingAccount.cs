ca4f30df95664fb39788125e5b7807070b641be8
Łukasz Nizioł <niziol.luk@gmail.com>
2020-11-25 14:50:38 +0100

    Adding a Billing Account class with basic code

---
+++ Bank/Bank/BillingAccount.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Bank
+{
+    class BillingAccount
+    {
+        public string AccuntNumber;
+        public decimal Balance;
+        public string FirstName;
+        public string LastName;
+        public long Pesel;
+    }
+}
