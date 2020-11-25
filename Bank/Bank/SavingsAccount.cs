ed39b972fb937e9a91878766898e615878d0ccaf
Łukasz Nizioł <niziol.luk@gmail.com>
2020-11-25 14:53:43 +0100

    Adding a SavingsAccount class with basic code

---
+++ Bank/Bank/SavingsAccount.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Bank
+{
+    class SavingsAccount
+    {
+        public string AccountNumber;
+        public decimal Balance;
+        public string FirstName;
+        public string LastName;
+        public long Pesel;
+    }
+}
