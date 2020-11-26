bfc85be366fa2d1632f68a9de9b791acf5d46a72
Łukasz Nizioł <niziol.luk@gmail.com>
2020-11-26 14:09:36 +0100

    Adding constructor and additional methods to BillingAccount and SavingAccount class

--- Bank/Bank/BillingAccount.cs
+++ Bank/Bank/BillingAccount.cs
@@ -13,5 +13,25 @@ namespace Bank
         public string FirstName;
         public string LastName;
         public long Pesel;
+    
+        public BillingAccount(string accountNumber, decimal balance, string firstName, string lastName, long pesel)
+        {
+            AccountNumber = accountNumber;
+            Balance = balance;
+            FirstName = firstName;
+            LastName = lastName;
+            Pesel = pesel;
+        }
+
+        public string GetFullName()
+        {
+            return string.Format("{0} {1}", FirstName, LastName);
+        }
+
+        public string GetBalance()
+        {
+            return string.Format("{0} zł", Balance);
+        }
+    
     }
 }
--- Bank/Bank/SavingsAccount.cs
+++ Bank/Bank/SavingsAccount.cs
@@ -13,5 +13,19 @@ namespace Bank
         public string FirstName;
         public string LastName;
         public long Pesel;
+
+        public SavingsAccount (string accountNumber, decimal balance, string firstName, string lastName, long pesel)
+        {
+            AccountNumber = accountNumber;
+            Balance = balance;
+            FirstName = firstName;
+            LastName = lastName;
+            Pesel = pesel;
+        }
+
+        public string GetFullName()
+        {
+            return string.Format("{0} {1}", FirstName, LastName);
+        }
     }
 }
