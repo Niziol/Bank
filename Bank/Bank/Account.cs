ec755abd15a7838aef80ccc11c9d88d50af0bf79
Łukasz Nizioł <niziol.luk@gmail.com>
2020-11-26 14:28:33 +0100

    Adding abstract class Account and modifying SavingAccount, BillingAccount class for this

---
+++ Bank/Bank/Account.cs
@@ -0,0 +1,35 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Bank
+{
+    abstract class Account
+    {
+        public string AccountNumber;
+        public decimal Balance;
+        public string FirstName;
+        public string LastName;
+        public long Pesel;
+        public Account(string accountNumber, decimal balance, string firstName, string lastName, long pesel)
+        {
+            AccountNumber = accountNumber;
+            Balance = balance;
+            FirstName = firstName;
+            LastName = lastName;
+            Pesel = pesel;
+        }
+
+        public string GetFullName()
+        {
+            return string.Format("{0} {1}", FirstName, LastName);
+        }
+
+        public string GetBalance()
+        {
+            return string.Format("{0} zł", Balance);
+        }
+    }
+}
--- Bank/Bank/BillingAccount.cs
+++ Bank/Bank/BillingAccount.cs
@@ -6,32 +6,11 @@ using System.Threading.Tasks;
 
 namespace Bank
 {
-    class BillingAccount
+    class BillingAccount : Account
     {
-        public string AccountNumber;
-        public decimal Balance;
-        public string FirstName;
-        public string LastName;
-        public long Pesel;
-    
         public BillingAccount(string accountNumber, decimal balance, string firstName, string lastName, long pesel)
+            : base(accountNumber, balance, firstName, lastName, pesel)
         {
-            AccountNumber = accountNumber;
-            Balance = balance;
-            FirstName = firstName;
-            LastName = lastName;
-            Pesel = pesel;
         }
-
-        public string GetFullName()
-        {
-            return string.Format("{0} {1}", FirstName, LastName);
-        }
-
-        public string GetBalance()
-        {
-            return string.Format("{0} zł", Balance);
-        }
-    
     }
 }
